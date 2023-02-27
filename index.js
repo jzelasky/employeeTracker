347ec1a604ce5157a7fef2972be46913350cfc3d
Jessica Zelasky <jzelasky@gmail.com>
2023-02-27 09:38:16 -0500

    modified menu and added new functionality

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const db = mysql.createConnection(
 );
 
 const log = console.log
-const table = console.table
+const table = (results) => {
+    log(' ');
+    console.table(results);
+    log(' ');
+}
 
 
 function init (){
@@ -28,33 +32,61 @@ function mainMenu(){
                 type: 'list',
                 message: 'What do you want to do?',
                 name: 'menu',
-                choices: ['View all departments','View all roles','View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+                choices: ['View Records', 'Add Record', 'Update Record','Delete Record','Exit']
             }
         ])
         .then((ans) => {
-            if(String(ans.menu) === 'View all departments'){
-                viewDepartments();
+            if(String(ans.menu) === 'View Records'){
+                viewMenu();
             }
-            if(String(ans.menu) === 'View all roles'){
-                viewRoles();
+            if(String(ans.menu) === 'Add Record'){
+                addMenu();
             }
-            if(String(ans.menu) === 'View all employees'){
-                viewEmployees();
+            if(String(ans.menu) === 'Update Record'){
+                updateMenu();
             }
-            if(String(ans.menu) === 'Add a department'){
-                addDepartment();
-            }
-            if(String(ans.menu) === 'Add a role'){
-                addRole();
-            }
-            if(String(ans.menu) === 'Add an employee'){
-                addEmployee();
-            }
-            if(String(ans.menu) === 'Update an employee role'){
-                updateRole();
+            if(String(ans.menu) === 'Delete Record'){
+                deleteMenu();
             }
             if(String(ans.menu) === 'Exit'){
                 process.exit();
+            }
+        })
+}
+
+//VIEW (SELECT) FUNCTIONS
+
+function viewMenu(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What do you want to do?',
+                name: 'menu',
+                choices: ['View all Departments','View all Roles','View all Employees','View Employees by Manager','View Employees by Department','View Budget by Department','Go Back to Main Menu']
+            }
+        ])
+        .then((ans) => {
+            if(String(ans.menu) === 'View all Departments'){
+                viewDepartments();
+            }
+            if(String(ans.menu) === 'View all Roles'){
+                viewRoles();
+            }
+            if(String(ans.menu) === 'View all Employees'){
+                viewEmployees();
+            }
+            if(String(ans.menu) === 'View Employees by Manager'){
+                viewEmpsByMngr();
+            }
+            if(String(ans.menu) === 'View Employees by Department'){
+                viewEmpsByDept();
+            }
+            if(String(ans.menu) === 'View Budget by Department'){
+                viewBudget();
+            }
+            if(String(ans.menu) === 'Go Back to Main Menu'){
+                mainMenu();
             }
         })
 }
@@ -63,9 +95,7 @@ function viewDepartments(){
     db.query(
         'SELECT * FROM department',
         function(err, results) {
-            log(' ');
             table(results);
-            log(' ')
             mainMenu();
         }
       );
@@ -75,9 +105,7 @@ function viewRoles(){
     db.query(
         'SELECT role.title, role.salary, department.name FROM role LEFT JOIN department ON department.id = role.department_id',
         function(err, results) {
-            log(' ')
             table(results);
-            log(' ')
             mainMenu();
         }
       );
@@ -85,14 +113,70 @@ function viewRoles(){
 
 function viewEmployees(){
     db.query(
-        'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, employee.manager_id FROM employee JOIN role ON role.id = employee.role_Id',
+        'SELECT * FROM employee JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id',
         function(err, results) {
-            log(' ')
             table(results);
-            log(' ')
             mainMenu();
         }
       );
+}
+
+function viewEmpsByMngr(){
+    db.query(
+        'SELECT * FROM employee ORDER BY manager_id',
+        function(err, results) {
+            table(results);
+            mainMenu();
+        }
+      );
+}
+
+function viewEmpsByDept(){
+    db.query(
+        'SELECT employee.first_name, employee.last_name, role.title, department.name FROM employee JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id ORDER BY department_id',
+        function(err, results) {
+            table(results);
+            mainMenu();
+        }
+      );
+}
+
+function viewBudget(){
+    db.query(
+        'SELECT department.name, SUM(role.salary) AS salary_sum FROM role JOIN department ON department.id = role.department_id GROUP BY role.department_id',
+        function(err, results){
+            table(results);
+            mainMenu();
+        }
+    )
+}
+
+//ADD (INSERT) FUNCTIONS
+
+function addMenu(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What do you want to do?',
+                name: 'menu',
+                choices: ['Add Department','Add Role','Add Employee','Go Back to Main Menu']
+            }
+        ])
+        .then((ans) => {
+            if(String(ans.menu) === 'Add Department'){
+                addDepartment();
+            }
+            if(String(ans.menu) === 'Add Role'){
+                addRole();
+            }
+            if(String(ans.menu) === 'Add Employee'){
+                addEmployee();
+            }
+            if(String(ans.menu) === 'Go Back to Main Menu'){
+                mainMenu();
+            }
+        })
 }
 
 function addDepartment(){
@@ -115,7 +199,7 @@ function addDepartment(){
                         db.query(
                             `INSERT INTO department (name) VALUES ("${ans.dept}")`,
                             function(err, results) {
-                              log(`${ans.name} department added`)
+                              log(`Department successfully added`)
                               mainMenu();
                             }
                         );
@@ -258,6 +342,31 @@ function addEmployee(){
         })
 }
 
+//UPDATE FUNCTIONS
+
+function updateMenu(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What do you want to do?',
+                name: 'menu',
+                choices: ["Update an Employee's Role","Update an Employee's Manager",'Go Back to Main Menu']
+            }
+        ])
+        .then((ans) => {
+            if(String(ans.menu) === "Update an Employee's Role"){
+                updateRole();
+            }
+            if(String(ans.menu) === "Update an Employee's Manager"){
+                updateManager();
+            }
+            if(String(ans.menu) === 'Go Back to Main Menu'){
+                mainMenu();
+            }
+        })
+}
+
 function updateRole(){
     inquirer
         .prompt([
@@ -316,6 +425,198 @@ function updateRole(){
             
         })
 }
+
+function updateManager (){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter employee's first name:",
+                name: 'empFname'
+            },
+            {
+                type: 'input',
+                message: "Enter employee's last name:",
+                name: 'empLname'
+            },
+            {
+                type: 'input',
+                message: "Enter new manager's first name:",
+                name: 'mngrFname'
+            },
+            {
+                type: 'input',
+                message: "Enter new manager's last name:",
+                name: 'mngrLname'
+            }  
+        ])
+        .then((ans) => {
+            ans.empFname, ans.empLname, ans.mngrFname, ans.mngrLname
+            let empId
+            let mngrId
+            db.query(
+                `SELECT id FROM employee WHERE first_name = '${ans.empFname}' AND last_name = '${ans.empLname}'`,
+                function(err, results) {
+                    if (results.length < 1){
+                        log('Error: Employee name not found')
+                        mainMenu();
+                    } else {
+                        empId = JSON.stringify(results[0]);
+                        empId = empId.slice(6, empId.length-1)
+                        empId = parseInt(empId);
+                    }
+                    db.query(
+                        `SELECT id FROM employee WHERE first_name = '${ans.mngrFname}' AND last_name = '${ans.mngrLname}'`,
+                        function(err, results) {
+                            if (results.length < 1){
+                                log('Error: Manager name not found')
+                                mainMenu();
+                            } else {
+                                mngrId = JSON.stringify(results[0]);
+                                mngrId = mngrId.slice(6, mngrId.length-1)
+                                mngrId = parseInt(mngrId);
+                            }
+                            db.query(
+                                `UPDATE employee SET manager_id = '${mngrId}' WHERE id = '${empId}'`,
+                                function (err, results) {
+                                    log("Employee's manager sucessfully updated");
+                                    mainMenu();
+                                }
+                            )
+                        }
+                    )
+                }
+            )
+        })
+}
+
+//DELETE FUNCTIONS
+
+function deleteMenu(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What do you want to do?',
+                name: 'menu',
+                choices: ['Delete a Department','Delete a Role','Delete an Employee','Go Back to Main Menu']
+            }
+        ])
+        .then((ans) => {
+            if(String(ans.menu) === 'Delete a Department'){
+                deleteDept();
+            }
+            if(String(ans.menu) === 'Delete a Role'){
+                deleteRole();
+            }
+            if(String(ans.menu) === 'Delete an Employee'){
+                deleteEmp();
+            }
+            if(String(ans.menu) === 'Go Back to Main Menu'){
+                mainMenu();
+            }
+        })
+}
+
+function deleteDept(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter name of department to delete:',
+                name: 'dept'
+            }
+        ])
+        .then((ans) => {
+            db.query(
+                `SELECT id FROM department WHERE name = '${ans.dept}'`,
+                function (err, results) {
+                    if (results.length < 1){
+                        log('Error: Department does not exists')
+                        mainMenu();
+                    } else {
+                        db.query(
+                            `DELETE FROM department WHERE department.name = '${ans.dept}'`,
+                            function(err, results) {
+                              log(`Department successfully deleted`)
+                              mainMenu();
+                            }
+                        );
+                    }
+                }
+            )
+        })
+}
+
+function deleteRole(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter name of role to delete:',
+                name: 'role'
+            }
+        ])
+        .then((ans) => {
+            db.query(
+                `SELECT id FROM role WHERE title = '${ans.role}'`,
+                function (err, results) {
+                    if (results.length < 1){
+                        log('Error: Role does not exists')
+                        mainMenu();
+                    } else {
+                        db.query(
+                            `DELETE FROM role WHERE role.title = '${ans.role}'`,
+                            function(err, results) {
+                              log(`Role successfully deleted`)
+                              mainMenu();
+                            }
+                        );
+                    }
+                }
+            )
+        })
+}
+
+function deleteEmp(){
+    log('hello')
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter employee to delete's first name:",
+                name: 'fName'
+            },
+            {
+                type: 'input',
+                message: "Enter employee to delete's last name:",
+                name: 'lName'
+            }
+        ])
+        .then((ans) => {
+            db.query(
+                `SELECT id FROM employee WHERE first_name = '${ans.fName}' AND last_name = '${ans.lName}'`,
+                function (err, results) {
+                    if (results.length < 1){
+                        log('Error: Employee does not exists')
+                        mainMenu();
+                    } else {
+                        let empId = JSON.stringify(results[0]);
+                        empId = empId.slice(6, empId.length-1)
+                        empId = parseInt(empId);
+                        db.query(
+                            `DELETE FROM employee WHERE employee.id = '${empId}'`,
+                            function(err, results) {
+                              log(`Employee successfully deleted`)
+                              mainMenu();
+                            }
+                        );
+                    }
+                }
+            )
+        })
+}
+
 
 init();
 
